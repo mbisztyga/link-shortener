c272a80eee4dbd9099ca12241f184f62d0eda170
mbisztyga <m.bisztyga777@gmail.com>
2024-06-23 10:29:29 +0200

    Changed line separators for dockerfiles, mvnw and maven-wrapper.properties, included required unix commands in dockerfiles and specified ports in dockerfiles and appliication .yml filles

--- save/src/main/java/com/gwsh/shortlink/save/data/KeyspaceRepository.java
+++ save/src/main/java/com/gwsh/shortlink/save/data/KeyspaceRepository.java
@@ -2,10 +2,14 @@ package com.gwsh.shortlink.save.data;
 
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
 import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
+import com.datastax.oss.driver.api.querybuilder.insert.Insert;
 import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
 import com.gwsh.shortlink.save.service.CassandraService;
 import lombok.RequiredArgsConstructor;
+
+import static com.gwsh.shortlink.save.common.CassandraVariables.KEYSPACE_1;
 
 @RequiredArgsConstructor
 public class KeyspaceRepository {
